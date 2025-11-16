530fb9e8db800d5507a3ea8860e4ea6d8fa6b178
rex <1073853456@qq.com>
2025-11-16 09:23:13 +0800

    update apis of executor

--- src/batch_executor/__init__.py
+++ src/batch_executor/__init__.py
@@ -2,11 +2,11 @@
 
 __author__ = """Rex Wang"""
 __email__ = '1073853456@qq.com'
-__version__ = '0.2.8'
+__version__ = '0.3.0'
 
 from .custom_logger import setup_logger
 from .main import (
-    Executor, batch_executor,
+    BatchExecutor, batch_executor,
     batch_async_executor, batch_hybrid_executor,
     batch_process_executor, batch_thread_executor
 )
